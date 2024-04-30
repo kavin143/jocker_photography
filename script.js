f176e9110eddfdc3ce84b837c667f7d4837a6e31
KAVIN <94214282+kavin143@users.noreply.github.com>
2024-04-30 20:45:46 +0530

    Files  or add

---
+++ script.js
@@ -0,0 +1,11 @@
+var sidenav=document.querySelector(".SideNav")
+
+function shownav(){
+   sidenav.style.left='0'
+}
+
+function closenav(){
+    sidenav.style.left='-60%'
+}
+
+// JavaScript code to handle navbar scrolling behavior
